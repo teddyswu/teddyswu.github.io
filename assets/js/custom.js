c31b003ea540948322604ba8d860a200dc0e0e67
steking1207 <steking1207@gmail.com>
2018-06-16 12:16:23 +0800

    180616-1

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$(function(){
+    $('.btn-anchor').click(function(){
+		$('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top}, 750);
+		return false;
+	});
+});
---
+++ assets/js/module.js
@@ -0,0 +1,3 @@
+$("#fixed-nav").load("module/fixed-nav.html");
+$("#sponsor").load("module/sponsor.html");
+$("#global-footer").load("module/global-footer.html");
