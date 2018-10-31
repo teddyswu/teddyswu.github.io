2454fa57d9b0963628cfe1e2e31e6e7bf03a36f0
steking1207 <steking1207@gmail.com>
2018-10-31 17:42:27 +0800

    181031

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -95,3 +95,43 @@ $('#menu').hcSticky({
 $.fn.datepicker.defaults.language = 'zh-TW';
 $('.input-daterange').datepicker({});
 $('.datepicker').datepicker();
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#support-action').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('#support-action').removeClass('scroll-up').addClass('scroll-down');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#support-action').removeClass('scroll-down').addClass('scroll-up');
+        }
+    }
+
+    lastScrollTop = st;
+}
